feb59f615c317d6aaac59ea97063b22a30ed7f3b
wolfreid <ecojem1234@gmail.com>
2022-12-11 17:31:19 +0200

    added projects' folder

---
+++ datamech2021/wrangling and tiydiing - quantificators.py
@@ -0,0 +1,12 @@
+'''Регулярные выражения'''
+
+# Работа с текстом, когда можна задать такую хитрую последовательность
+
+print("hell\?o")
+# The monkey ate a (rotten )? banana
+# The monkey ate a rotten banana 
+# The monkey ate a banana.
+#(1 )?(5 )?(7 )?duck(s)? for adoption\?
+# hoo+o*t
+# ^penguins are cooler than regular expressions$
+# ^[\(|\)(\d| )*[0-9.-]*
